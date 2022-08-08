dda558104f28ff6eebec469a5ca9f78f16dc234e
devshakti78 <shakti.dev46@gmail.com>
2022-08-08 13:12:36 +0530

    added headers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Header from './Components/Header';
 import List from './Components/List';
 
 function App() {
   return (
     <div>
+      <Header/>
       <List/>
     
     </div>
